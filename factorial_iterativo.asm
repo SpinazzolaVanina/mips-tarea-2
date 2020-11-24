641cef2c8552c3f9f8d8267a510336946aed80f7
SpinazzolaVanina <67491614+SpinazzolaVanina@users.noreply.github.com>
2020-11-24 14:13:57 -0300

    Update factorial_iterativo.asm

--- factorial_iterativo.asm
+++ factorial_iterativo.asm
@@ -1,31 +1,49 @@
 .data
-prompt: .asciiz "Ingrese un numero: "
+ingreso: .asciiz "Ingrese un numero: "
 
 .text
 .globl main
-
-# --- funcion main ---
 main:
-li       $v0, 4              #
-la       $a0, prompt         #
-syscall                      # printf("Ingrese un numero: ");
-li       $v0, 5              #
-syscall                      # scanf("%d", &n);
-move     $a0, $v0            #
-jal      factorial           # factorial(n);
-move     $a0, $v0            #
-li       $v0, 1              #
-syscall                      # printf("%d\n", res);
-li       $v0, 10             #
-syscall                      #
 
-# --- funcion factorial ---
+#Pido al usuario un numero y lo guardo en a0
+	li $v0, 4			
+	la $a0, ingreso		
+	syscall		
+	li $v0, 5			
+	syscall				
+	move $a0, $v0		
+
+#Llamo a factorial
+jal factorial
+
+#Imprimo el resultado
+move      $a0, $v0
+li        $v0, 1
+syscall
+
+
+#Finalizo
+li $v0, 10
+syscall
+
+
 factorial:
-li       $v0, 1              # int resultado = 1;
+#Prologo
+	addi $sp, $sp, -4 	#Un solo lugar: a0
+	sw $a0, 0($sp)
+#-------
+	li $s0, 1			#El resultado
 loop:
-beq      $a0, $zero, exit    # while (n > 0)
-mul      $v0, $a0, $v0       # resultado *= n;
-addi     $a0, $a0, -1        # n--;
-j        loop                #
-exit:
-jr       $ra                 # return
+	beq $a0, $zero, return		#while(t0 != 0){
+	mul $s0, $s0, $a0			#	t1 *= t0;
+	addi $a0, $a0, -1			#	t0--;
+	j loop						#}
+
+return:
+	move $v0, $s0
+
+#Epilogo
+	lw $a0, 0($sp)
+	addi $sp, $sp, 4
+#-----
+	jr $ra 				#Return
